e60a63b8d7e380035d5dd7887fcf0ef4f88cb780
DALPORTODEV <christopher.dalporto@outlook.com>
2024-09-26 10:14:25 -0700

    add ping test

---
+++ ping_test.py
@@ -0,0 +1,26 @@
+from re import findall
+from subprocess import Popen, PIPE
+
+def ping (host,ping_count):
+
+    for ip in host:
+        data = ""
+        output= Popen(f"ping {ip} -n {ping_count}", stdout=PIPE, encoding="utf-8")
+
+        for line in output.stdout:
+            data = data + line
+            ping_test = findall("TTL", data)
+
+        if ping_test:
+            print(f"{ip} : Successful Ping")
+            return True
+        else:
+            print(f"{ip} : Failed Ping")
+            return False
+
+nodes = ["192.168.50.130"]
+
+#ping(nodes,3)
+
+def test_ping():
+    assert ping(nodes, 3) == True, "PING FAILED"
